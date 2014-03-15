43d52c96ecf8d0cc82641901de0e4a08fd39875e
Nexelen <nexelen@gmail.com>
2014-03-16 01:10:06 +0400

    Add import from QML

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 //#include "window.h"                 //basic window with OpenGL context 4.3 Core profile
 #include <QApplication>
 #include <QQmlContext>
+#include <QQmlProperty>
+#include <QQuickItem>
 
 
 int main(int argc, char* argv[])
@@ -11,12 +13,15 @@ int main(int argc, char* argv[])
     QuickWindow* window = new QuickWindow();
     //window->setPosition(200, 100);
     window->setMinimumSize( QSize( 1024, 612 ) );
-    window->rootContext()->setContextProperty("windowq", window);
+    window->rootContext()->setContextProperty("windowq", window);       //In QML
     window->setResizeMode( QQuickView::SizeRootObjectToView );
     window->setTitle("Qt5 OpenGL 4.3 Study");
     //window->setFlags(Qt::FramelessWindowHint);
     window->setSource(QUrl("qrc:///qml/main.qml"));
     window->show();
 
+    QQuickItem *object = window->rootObject();                          //From QML
+    QObject *text = object->findChild<QObject*>("text1");
+    qDebug() << "Property value:" << QQmlProperty::read(text, "text").toString();
     return app.exec();
 }
