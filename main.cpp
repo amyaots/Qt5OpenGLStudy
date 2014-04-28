f58819fce952f47f8f4d3eedd6d838ba4efedbee
Nexelen <nexelen@gmail.com>
2014-04-28 22:18:20 +0400

    change window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "quickwindow.h"              //QtQuick Window with OpenGL conext 4.3 Core profile
-//#include "window.h"                 //basic window with OpenGL context 4.3 Core profile
+//#include "quickwindow.h"              //QtQuick Window with OpenGL conext 4.3 Core profile
+#include "window.h"                 //basic window with OpenGL context 4.3 Core profile
 #include <QApplication>
 
 
@@ -7,13 +7,13 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    QuickWindow* window = new QuickWindow();
+    Window* window = new Window();
     //window->setPosition(200, 100);
     window->setMinimumSize( QSize( 512, 512 ) );
-    window->setResizeMode( QQuickView::SizeRootObjectToView );
+    //window->setResizeMode( QQuickView::SizeRootObjectToView );
     window->setTitle("Qt5 OpenGL 4.3 Study");
     //window->setFlags(Qt::FramelessWindowHint);
-    window->setSource(QUrl("qrc:///qml/main.qml"));
+    //window->setSource(QUrl("qrc:///qml/main.qml"));
     window->show();
 
     return app.exec();
